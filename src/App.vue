390346bf1b97947c38051e69cbfbf373270e11a2
Anton Koksharov <anton.koksharov@outlook.com>
2024-09-17 16:34:19 +0300

    feat: API connectivity test

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@ import apiClient from "@/plugins/apiClient.js";
 export default defineComponent({
   setup() {
 
-    onMounted(() => {
-      apiClient.get('/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc')
+    onMounted(async () => {
+      const { data } = await apiClient.get('/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc')
+      console.log(data)
     })
 
     return {}
