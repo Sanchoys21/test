225c6902b935a0953f075360048335da9233305b
Sanchoys21 <sashaitonya@gmail.com>
2024-09-18 14:39:24 +0200

    styles and film card component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,182 @@
 <script>
-import {defineComponent, onMounted} from "vue";
+import {defineComponent, onMounted, ref} from "vue";
 import apiClient from "@/plugins/apiClient.js";
+import FilmCard from "@/components/Film-card.vue";
 
 export default defineComponent({
+  components: {FilmCard},
   setup() {
 
+    const trending = ref([])
+
     onMounted(async () => {
-      const { data } = await apiClient.get('/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc')
+      const {data} = await apiClient.get('/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc')
       console.log(data)
+      trending.value = data.results
     })
 
-    return {}
+
+    return {
+      trending
+    }
   }
 })
 </script>
 
 <template>
-  <main>
+  <div class="page">
+    <aside class="sidebar-streaming">
+      <!--      icons-->
+    </aside>
+    <aside class="sidebar">
+      <div class="menu">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Discovery</a></li>
+          <li><a href="#">Community</a></li>
+          <li><a href="#">Coming soon</a></li>
+        </ul>
+      </div>
+      <div class="library">
+        <ul>
+          <li><a href="#">Recent</a></li>
+          <li><a href="#">Bookmarked</a></li>
+          <li><a href="#">Top Rated</a></li>
+          <li><a href="#">Downloaded</a></li>
+        </ul>
+      </div>
+      <div class="settings">
+        <ul>
+          <li><a href="#">Settings</a></li>
+          <li><a href="#">Help</a></li>
+        </ul>
+      </div>
+      <button class="logout">Logout</button>
+    </aside>
+    <main>
+      <div class="top-bar">
+        <ul>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">Series</a></li>
+          <li><a href="#">TV Shows</a></li>
+        </ul>
+      </div>
+      <section class="trending">
+        <h2>Trending Movies</h2>
+        <div class="movie-list">
+          <film-card v-for="(movie, index) in trending"
+                     :key="index"
+                     :title="movie.title"
+                     :year="movie.release_date.split('-')[0]"
+          ></film-card>
+        </div>
+      </section>
+      <section class="top">
+        <h2>Top Rated</h2>
+        <div class="movie-list">
+          <film-card v-for="(movie, index) in trending"
+                     :key="index"
+                     :title="movie.title"
+                     :year="movie.release_date.split('-')[0]"
+          ></film-card>
+        </div>
+      </section>
+    </main>
+    <aside class="sidebar-right">
+      <div class="profile-data"></div>
 
-  </main>
+    </aside>
+  </div>
 </template>
 
 <style scoped>
+.page {
+  display: flex;
+  background: #0D0D0F;
+  color: white;
+  height: 100vh;
+  font-family: sans-serif;
+}
+
+.sidebar-streaming {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 5%;
+  padding-top: 5%;
+}
+
+.sidebar, .sidebar-right {
+  background: #1A171E;
+  width: 10%;
+  padding: 20px;
+}
+
+.sidebar {
+  display: flex;
+  flex-direction: column;
+  padding-top: 5%;
+}
+
+.sidebar ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.sidebar li {
+  margin: 15px 0;
+}
+
+.sidebar li a {
+  text-decoration: none;
+  color: #6F6E74;
+  border: none;
+  cursor: pointer;
+}
+
+.menu, .library {
+  border-bottom: #6F6E74 1px solid;
+}
+
+main {
+  flex: 1;
+  padding: 0 20px 20px;
+  overflow-y: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+
+main::-webkit-scrollbar {
+  display: none;
+}
+
+.top-bar ul {
+  display: flex;
+  list-style: none;
+  gap: 40px;
+  padding: 0;
+}
+
+.top-bar ul li a {
+  text-decoration: none;
+  color: #6F6E74;
+}
+
+button {
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  background: #1A171E;
+  color: #6F6E74;
+  text-align: left;
+  margin: auto 0 0;
+}
+
+.movie-list {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  overflow-x: auto;
+}
+
 </style>
---
+++ src/components/Film-card.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: "Film-card",
+  props: {
+    title: String,
+    year: Number
+  }
+}
+</script>
+
+<template>
+  <div class="card">
+    <div class="content">
+      <h3 class="title">{{ title }}</h3>
+      <p class="year">{{ year }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  flex: 0 0 auto;
+  border: 1px solid #000;
+  border-radius: 10px;
+  width: 250px;
+  height: 100px;
+  padding: 100px 20px 20px 20px;
+  background: gray;
+}
+.card h1, .card p {
+  padding: 0;
+  margin: 0;
+}
+
+</style>
