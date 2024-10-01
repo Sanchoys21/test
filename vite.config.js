544011dc0eb97ffe481338b09b9cd720c5bba4ca
Sanchoys21 <sashaitonya@gmail.com>
2024-10-01 15:48:54 +0200

    github pages preparation 3

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://api.themoviedb.org/3',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    server: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'https://api.themoviedb.org/3',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    base: '/test/'
+});
